444cf5d02f764053799c3144ddff06221c1d4bd3
coderbee <wen866595@163.com>
2019-11-06 21:21:26 +0800

    合并K个有序链表优化：参考合并排序思路，两两合并，减少总的合并次数为 lg2(N)，N 为要合并的链表数量。

--- src/main/java/net/coderbee/leetcode/algorithm/list/MergeKSortedLists.java
+++ src/main/java/net/coderbee/leetcode/algorithm/list/MergeKSortedLists.java
@@ -2,6 +2,48 @@ package net.coderbee.leetcode.algorithm.list;
 
 public class MergeKSortedLists {
 	public ListNode mergeKLists(ListNode[] lists) {
+		if (lists == null || lists.length < 1) {
+			return null;
+		}
+
+		int end = lists.length - 1;
+		while (end > 0) {
+			int begin = 0;
+			while (begin < end) {
+				lists[begin] = mergeTwoLists(lists[begin], lists[end]);
+				begin++;
+				end--;
+			}
+		}
+
+		return lists[0];
+	}
+
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(0), p = head;
+
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				p = p.next = l1;
+				l1 = l1.next;
+			} else {
+				p = p.next = l2;
+				l2 = l2.next;
+			}
+		}
+
+		if (l1 != null) {
+			p.next = l1;
+		}
+
+		if (l2 != null) {
+			p.next = l2;
+		}
+
+		return head.next;
+	}
+
+	public ListNode mergeKListsByLoop(ListNode[] lists) {
 		ListNode head = new ListNode(0);
 		mergeInner(head, lists);
 		return head.next;
