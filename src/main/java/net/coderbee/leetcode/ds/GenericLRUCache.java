f2f451c9dd9dba8b92b081ef54d56f25abfb5363
coderbee <wen866595@163.com>
2019-11-13 21:41:54 +0800

    LRU缓存机制
    https://leetcode-cn.com/problems/lru-cache

---
+++ src/main/java/net/coderbee/leetcode/ds/GenericLRUCache.java
@@ -0,0 +1,77 @@
+package net.coderbee.leetcode.ds;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenericLRUCache<K, V> {
+	Map<K, ListNode> map = new HashMap<>();
+	final int capacity;
+	int size = 0;
+	ListNode head;
+	ListNode tail;
+
+	public GenericLRUCache(int capacity) {
+		this.capacity = capacity;
+		head = new ListNode(null, null);
+		tail = new ListNode(null, null);
+		head.next = tail;
+		tail.prev = head;
+	}
+
+	public V get(K key) {
+		ListNode node = map.get(key);
+		if (node == null) {
+			return null;
+		}
+		dequeue(node);
+		enqueue(node);
+		return node.val;
+	}
+
+	public void put(K key, V value) {
+		ListNode node = map.get(key);
+		if (node == null) {
+			if (size >= capacity) {
+				ListNode h = head.next;
+				dequeue(h);
+				map.remove(h.key);
+				size--;
+			}
+			node = new ListNode(key, value);
+			map.put(key, node);
+			enqueue(node);
+			size++;
+		} else {
+			node.val = value;
+			dequeue(node);
+			enqueue(node);
+		}
+	}
+
+	private void enqueue(ListNode node) {
+		tail.prev.next = node;
+		node.prev = tail.prev;
+		node.next = tail;
+		tail.prev = node;
+	}
+
+	private void dequeue(ListNode node) {
+		node.prev.next = node.next;
+		node.next.prev = node.prev;
+		node.next = null;
+		node.prev = null;
+	}
+
+	class ListNode {
+		K key;
+		V val;
+		ListNode next;
+		ListNode prev;
+
+		public ListNode(K key, V val) {
+			this.key = key;
+			this.val = val;
+		}
+	}
+
+}
