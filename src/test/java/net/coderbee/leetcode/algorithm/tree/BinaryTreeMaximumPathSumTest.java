47e4388387be1dbef36eb7efbe3a4cc683046061
coderbee <wen866595@163.com>
2019-12-01 21:49:29 +0800

    124. 二叉树中的最大路径和---官方答案。

--- src/test/java/net/coderbee/leetcode/algorithm/tree/BinaryTreeMaximumPathSumTest.java
+++ src/test/java/net/coderbee/leetcode/algorithm/tree/BinaryTreeMaximumPathSumTest.java
@@ -11,12 +11,14 @@ public class BinaryTreeMaximumPathSumTest {
 		BinaryTreeMaximumPathSum obj = new BinaryTreeMaximumPathSum();
 
 		assertEquals(-3, obj.maxPathSum(new TreeNode(-3)));
-
 		assertEquals(6, obj.maxPathSum(Codec.deserialize("[1,2,3]")));
-
 		assertEquals(42, obj.maxPathSum(Codec.deserialize("[-10,9,20,null,null,15,7]")));
-
 		assertEquals(16, obj.maxPathSum(Codec.deserialize("[9,6,-3,null,null,-6,2,null,null,2,null,-6,-6,-6]")));
+
+		assertEquals(-3, obj.maxPathSum0(new TreeNode(-3)));
+		assertEquals(6, obj.maxPathSum0(Codec.deserialize("[1,2,3]")));
+		assertEquals(42, obj.maxPathSum0(Codec.deserialize("[-10,9,20,null,null,15,7]")));
+		assertEquals(16, obj.maxPathSum0(Codec.deserialize("[9,6,-3,null,null,-6,2,null,null,2,null,-6,-6,-6]")));
 	}
 
 }
