9d2d8aa9e2d884a94f17668851ad8462ce60932f
coderbee <wen866595@163.com>
2019-12-03 21:52:10 +0800

    39. 组合总和
    https://leetcode-cn.com/problems/combination-sum

---
+++ src/main/java/net/coderbee/leetcode/algorithm/combination/CombinationSum.java
@@ -0,0 +1,51 @@
+package net.coderbee.leetcode.algorithm.combination;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 39. 组合总和
+ * 
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * 
+ * candidates 中的数字可以无限制重复被选取。
+ * 
+ * 说明：
+ * 
+ * 所有数字（包括 target）都是正整数。 解集不能包含重复的组合。
+ * 
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/combination-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CombinationSum {
+
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> rs = new LinkedList<List<Integer>>();
+		recursive(rs, new ArrayList<Integer>(), 0, candidates, target);
+		return rs;
+	}
+
+	private static void recursive(List<List<Integer>> rs, List<Integer> tmp, int tmpSum, int[] candidates, int target) {
+		if (tmpSum == target) {
+			tmp.sort(Integer::compareTo);
+			if (!rs.contains(tmp)) {
+				rs.add(tmp);
+			}
+			return;
+		}
+
+		if (tmpSum > target) {
+			return;
+		}
+
+		for (int i : candidates) {
+			if (tmpSum + i <= target) {
+				List<Integer> list = new ArrayList<>(tmp);
+				list.add(i);
+				recursive(rs, list, tmpSum + i, candidates, target);
+			}
+		}
+	}
+
+}
