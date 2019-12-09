6b990a7b5cba8c18dc5a890f5078b6011c5056cb
coderbee <wen866595@163.com>
2019-12-09 23:58:29 +0800

    42. 接雨水
    把顶点数据保存在额外的数组里，简化数据。

--- src/main/java/net/coderbee/leetcode/algorithm/area/TrappingRainWater.java
+++ src/main/java/net/coderbee/leetcode/algorithm/area/TrappingRainWater.java
@@ -2,6 +2,77 @@ package net.coderbee.leetcode.algorithm.area;
 
 public class TrappingRainWater {
 
+	public static int trapCached(int[] height) {
+		if (height == null || height.length <= 1) {
+			return 0;
+		}
+
+		int[] cache = new int[height.length];
+		// 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1
+		cache[0] = height[0];
+		for (int i = 1; i < height.length; i++) {
+			if (height[i] > height[i - 1]) {
+				cache[i - 1] = 0;
+				cache[i] = height[i];
+			} else if (height[i] < height[i - 1]) {
+				cache[i] = 0;
+			}
+		}
+
+		return calc(height, cache);
+	}
+
+	private static int calc(int[] height, int[] cache) {
+		int start = 0;
+		while (start < cache.length && cache[start] == 0) {
+			start++;
+		}
+		if (start == cache.length) {
+			return 0;
+		}
+
+		int area = 0;
+		do {
+			int end = -1;
+			int pos = start + 1;
+			while (pos < cache.length) {
+				if (cache[pos] != 0) {
+					if (end == -1) {
+						end = pos;
+					}
+
+					if (cache[pos] >= cache[start]) {
+						end = pos;
+						break;
+					} else if (cache[pos] > cache[end]) {
+						end = pos;
+					}
+				}
+
+				pos++;
+			}
+			if (end == -1) {
+				break;
+			}
+
+			area += area(height, start, end);
+
+			start = end;
+		} while (start < cache.length);
+
+		return area;
+	}
+
+	private static int area(int[] height, int regionStart, int regionEnd) {
+		int h = Math.min(height[regionStart], height[regionEnd]);
+		int area = (regionEnd - regionStart - 1) * h;
+		for (int i = regionStart + 1; i < regionEnd; i++) {
+			area -= Math.min(height[i], h);
+		}
+		System.out.println("regionStart:" + regionStart + ", regionEnd:" + regionEnd + ", area:" + area);
+		return area;
+	}
+
 	public static int trap(int[] height) {
 		if (height == null || height.length == 1) {
 			return 0;
@@ -44,16 +115,6 @@ public class TrappingRainWater {
 		}
 
 		return area(height, regionStart, regionEnd) + trap(height, regionEnd);
-	}
-
-	private static int area(int[] height, int regionStart, int regionEnd) {
-		int h = Math.min(height[regionStart], height[regionEnd]);
-		int area = (regionEnd - regionStart - 1) * h;
-		for (int i = regionStart + 1; i < regionEnd; i++) {
-			area -= Math.min(height[i], h);
-		}
-		System.out.println("regionStart:" + regionStart + ", regionEnd:" + regionEnd + ", area:" + area);
-		return area;
 	}
 
 	private static int findUpside(int[] height, int start) {
--- src/test/java/net/coderbee/leetcode/algorithm/area/TrappingRainWaterTest.java
+++ src/test/java/net/coderbee/leetcode/algorithm/area/TrappingRainWaterTest.java
@@ -5,6 +5,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TrappingRainWaterTest {
+	@Test
+	public void testTrapCached() throws Exception {
+		assertEquals(6, TrappingRainWater.trapCached(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+
+		assertEquals(1, TrappingRainWater.trapCached(new int[] { 5, 4, 1, 2 }));
+
+		assertEquals(14, TrappingRainWater.trapCached(new int[] { 5, 2, 1, 2, 1, 5 }));
+
+		assertEquals(83, TrappingRainWater.trapCached(
+				new int[] { 6, 4, 2, 0, 3, 2, 0, 3, 1, 4, 5, 3, 2, 7, 5, 3, 0, 1, 2, 1, 3, 4, 6, 8, 1, 3 }));
+
+		assertEquals(3, TrappingRainWater.trapCached(new int[] { 9, 6, 8, 8, 5, 6, 3 }));
+
+	}
 
 	@Test
 	public void test() throws Exception {
