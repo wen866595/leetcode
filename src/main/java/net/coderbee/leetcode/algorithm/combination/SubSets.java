2bcf6669cf2256f83d5dd963b6d3db2cf9e88159
coderbee <wen866595@163.com>
2021-06-26 11:50:30 +0800

    78. 子集

--- src/main/java/net/coderbee/leetcode/algorithm/combination/SubSets.java
+++ src/main/java/net/coderbee/leetcode/algorithm/combination/SubSets.java
@@ -4,26 +4,48 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 78. 子集
+ *
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ *
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ */
 public class SubSets {
 
 	public static List<List<Integer>> subsets(int[] nums) {
-		List<List<Integer>> rs = new LinkedList<List<Integer>>();
+		List<List<Integer>> rs = new LinkedList<>();
 
-		recursive(rs, new ArrayList<Integer>(), nums, 0);
+		backtrace(rs, new ArrayList<>(), nums, 0);
 		return rs;
 	}
 
-	private static void recursive(List<List<Integer>> rs, List<Integer> list, int[] nums, int index) {
+	private static void backtrace(List<List<Integer>> rs, ArrayList<Integer> list, int[] nums, int index) {
+		rs.add(new ArrayList<>(list));
+		for(int i = index; i < nums.length; i++) {
+			list.add(nums[i]);
+			backtrace(rs, list, nums, i + 1);
+			list.remove(list.size() - 1);
+		}
+	}
+
+	public static List<List<Integer>> subsets0(int[] nums) {
+		List<List<Integer>> rs = new LinkedList<>();
+
+		dfs(rs, new ArrayList<>(), nums, 0);
+		return rs;
+	}
+
+	private static void dfs(List<List<Integer>> rs, ArrayList<Integer> list, int[] nums, int index) {
 		if (index >= nums.length) {
-			rs.add(list);
+			rs.add(new ArrayList<>(list));
 			return;
 		}
 
-		List<Integer> newList = new ArrayList<Integer>(list);
-		newList.add(nums[index]);
-		recursive(rs, newList, nums, index + 1);
+		dfs(rs, list, nums, index + 1);
 
-		recursive(rs, list, nums, index + 1);
+		ArrayList<Integer> next = new ArrayList<>(list);
+		next.add(nums[index]);
+		dfs(rs, next, nums, index + 1);
 	}
-
 }
