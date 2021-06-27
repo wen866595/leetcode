afe81a2e4644b489deb4134266b2a06a731e9f2b
coderbee <wen866595@163.com>
2021-06-27 16:43:51 +0800

    104. 二叉树的最大深度

---
+++ src/main/java/net/coderbee/leetcode/algorithm/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,25 @@
+package net.coderbee.leetcode.algorithm.tree;
+
+/**
+ * 104. 二叉树的最大深度
+ * <p>
+ * 给定一个二叉树，找出其最大深度。
+ * <p>
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+public class MaximumDepthOfBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+    }
+
+}
